934d819c9c3e66a70b91229b4a46350a41c1a0fd
Gaza-Israel <71357732+Gaza-Israel@users.noreply.github.com>
2022-05-30 15:49:42 +0100

    Added segments filter and changed plot_lines to handle DFs

--- bag_reader.py
+++ bag_reader.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 from math import cos,sin
 from skimage.feature import peak_local_max
 from scipy import ndimage, misc
+import matplotlib.pyplot
 
 def load_bag(name):
     b = bagreader(name)
 
     # get the list of topics
     table_info = b.topic_table
-
+    
     LASER_MSG = b.message_by_topic('/scan')
     df_laser = pd.read_csv(LASER_MSG)
         
@@ -51,12 +52,14 @@ def polar2cart(d,theta): #inputs as array
 
 
 class feature_detector:
-    def __init__(self,phi_resolution = 0.1,r_resolution = 0.01,convolution_filter_size = 5,min_th = 20):
+    def __init__(self,phi_resolution = 0.1,r_resolution = 0.01,convolution_filter_size = 5,min_th = 20,threshold_error = 0.01,threshold_npoints = 20):
         self.phi_resolution =  phi_resolution
         self.r_resolution = r_resolution 
         self.Max_dist = 0
         self.min_th = min_th
         self.convolution_filter_size = convolution_filter_size
+        self.threshold_error = threshold_error
+        self.threshold_npoints = threshold_npoints
         
     def hough_transform(self,x,y):
         Nx = np.max(x)-np.min(x)
@@ -75,36 +78,38 @@ class feature_detector:
     def detect_peaks(self,accumulator):            
         acc_peaks = peak_local_max(accumulator, min_distance=35,threshold_abs=self.min_th,threshold_rel=0.15, exclude_border=False)
         return acc_peaks
+    
     def extract_segments(self,x,y,rs,phis,th):
         points = np.vstack((x,y)).T
         endpoints = np.zeros((phis.shape[0],4))
         npoints = np.zeros(phis.shape)
         error_mse = np.zeros(phis.shape)
-        i = np.array([[1,0],[0,1]])
         for idx,phi in enumerate(phis):
             b = rs[idx]/np.sin(phis[idx])
             p = np.array([[0,b]]).T
             v = np.array([[-np.sin(phis[idx]),np.cos(phis[idx])]]).T
               
+            
+            
             dist = np.linalg.norm(np.reshape(np.cross((points-p.T),v.T), (-1, 1)),axis = 1)
             keep_idx = np.where(np.abs(dist)<th)[0]
             npoints[idx]=keep_idx.shape[0]
-            if npoints[idx] != 0:
             
-                projection_matrix = np.dot(i.T,v)/np.dot(v.T,v)*v.T
-                p_projected = p-np.dot(projection_matrix,p)
-                P = np.hstack((projection_matrix,p_projected))
-                points_projected = np.dot(P,np.hstack((points[keep_idx],np.ones((points[keep_idx].shape[0],1)))).T).T
-                error_mse[idx] = (dist[keep_idx]**2).mean()
-                if phis[idx]==np.deg2rad(90):
-                    max_projected = np.argmax(points_projected[:,1],axis = 0)
-                    min_projected = np.argmin(points_projected[:,1],axis = 0)
-                    endpoints[idx,:] = np.hstack((points_projected[max_projected,:],points_projected[min_projected,:]))
-                else:
-                    max_projected = np.argmax(points_projected[:,0],axis = 0)
-                    min_projected = np.argmin(points_projected[:,0],axis = 0)
-                    endpoints[idx,:] = np.hstack((points_projected[max_projected,:],points_projected[min_projected,:]))
-                
+            
+            i = np.array([[1,0],[0,1]])
+            projection_matrix = np.dot(i.T,v)/np.dot(v.T,v)*v.T
+            p_projected = p-np.dot(projection_matrix,p)
+            P = np.hstack((projection_matrix,p_projected))
+            points_projected = np.dot(P,np.hstack((points[keep_idx],np.ones((points[keep_idx].shape[0],1)))).T).T
+            error_mse[idx] = (dist[keep_idx]**2).mean()
+            if phis[idx]==np.deg2rad(90):
+                max_projected = np.argmax(points_projected[:,1],axis = 0)
+                min_projected = np.argmin(points_projected[:,1],axis = 0)
+                endpoints[idx,:] = np.hstack((points_projected[max_projected,:],points_projected[min_projected,:]))
+            else:
+                max_projected = np.argmax(points_projected[:,0],axis = 0)
+                min_projected = np.argmin(points_projected[:,0],axis = 0)
+                endpoints[idx,:] = np.hstack((points_projected[max_projected,:],points_projected[min_projected,:]))
         
         df = pd.DataFrame(endpoints,columns=['x_1','y_1','x_2','y_2'])
         df['rs_line'] = rs
@@ -113,8 +118,10 @@ class feature_detector:
         df['error_mse'] = error_mse
         
         return endpoints,df
-    def filter_segments(self):
-        a = 1
+    def filter_segments(self,df):
+        df = df[df['error_mse']<=self.threshold_error]
+        df = df[df['npoints']>=self.threshold_npoints]
+        return df
     def find_intersections(self):
         a = 1
     def extract_features(self):
@@ -125,26 +132,11 @@ class feature_detector:
         plt.imshow(accumulator)
         plt.set_cmap('gray')
         plt.show()
-    def plot_lines(self,x,y,r = None,phi = None,endpoints = None):
-        fig = plt.figure()
-        plt.plot(x,y,'k.',markersize=10)
-        if phi is not None and r is not None:
-            for idx in range(phi.shape[0]):
-                if phis[idx]==np.deg2rad(90):
-                    a = 1
-                else:
-                    x1 = np.linspace(endpoints[idx,0],endpoints[idx,2],100)
-                    a = -1/np.tan(phi[idx])
-                    b = r[idx]/np.sin(phi[idx])
-                    
-                    y1 = a*x1+b
-                    plt.plot(x1,y1)
-        # plt.xlim(0.8, 1.2)
-        plt.ylim(-0.5,10)
-        # display the Polar plot
-        plt.show()
-
-
+    def plot_lines(self,x,y,df=None):
+        
+        plt.figure()
+        plt.scatter(x,y,color = 'k',linewidths=0.5)
+        plt.plot([df['x_1'],df['x_2']],[df['y_1'],df['y_2']])
 
 
 df_laser = load_bag('./23-05/2022-05-23-15-50-47.bag')
@@ -169,11 +161,14 @@ ft.plot_hough_space(nx, ny)
 
 accumulator, phis, rs = ft.hough_transform(nx,ny)
 acc_peaks = ft.detect_peaks(accumulator)
-endpoints, df_lines_info = ft.extract_segments(nx, ny, rs[acc_peaks[:,0]],phis[acc_peaks[:,1]],0.1)
+endpoints, df_lines_info = ft.extract_segments(nx, ny, rs[acc_peaks[:,0]],phis[acc_peaks[:,1]],0.3)
 
-ft.plot_lines(nx, ny,rs[acc_peaks[:,0]],phis[acc_peaks[:,1]],endpoints)
+ft.plot_lines(nx, ny,df_lines_info)
 
 
+df_lines_info_segments = ft.filter_segments(df_lines_info)
 
+
+ft.plot_lines(nx, ny,df_lines_info_segments)
 
 
