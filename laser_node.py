addd7e1cfce5120638cffa280d969446651e8261
Gaza-Israel <israelsother@usp.br>
2022-06-19 22:25:58 +0100

    Fixed some type errors

--- laser_node.py
+++ laser_node.py
@@ -1,63 +1,83 @@
 #!/usr/bin/env python
 import rospy
 import geometry_msgs.msg
-import std_msgs
+import copy
 import numpy as np
+from io import BytesIO
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import PoseWithCovarianceStamped
 from visualization_msgs.msg import Marker
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from range_finder_features_pkg.msg import features_msg
-class laser_node:
+from Feature_detector import feature_detector, feature_matcher,polar2z
+class Laser_node:
     def __init__(self):
         self.laser_features_publisher = rospy.Publisher("laser_features",features_msg,queue_size=1)
+        self.fd = feature_detector(laser_max_range=5.6, res_map=0.01, acc_th=20, min_line_lenght=0.30, max_line_gap=0.30, min_dist2line_th=0.2, filter_min_points = 20,threshold_cluster=0.02,max_intersection_distance=8)
+        self.fm = feature_matcher(0.2)
 
+    def extract_laser_data_from_message(self,data:LaserScan):
+        angle_array = np.arange(data.angle_min, data.angle_max + data.angle_increment, data.angle_increment)
+        ranges = np.array(data.ranges)
+        array_has_nan = np.logical_or(np.isnan(data.ranges), ranges<0.05)
+        data.ranges = ranges[np.invert(array_has_nan)]
+        angle_array = angle_array[np.invert(array_has_nan)]
+
+        return (data.ranges, angle_array)
     def pub_laser_features(self,features,feature_idx):
         msg = features_msg()
-        for feature_tuple in zip(features,feature_idx):
+        for feature_tuple in zip(features['x'],features['y'],feature_idx):
             p = geometry_msgs.msg.Point()
-            p.x = feature_tuple[0][0]
-            p.y = feature_tuple[0][1]
-            p.z = feature_tuple[1]
-            self.msg.points.append(p)
+            p.x = feature_tuple[0]
+            p.y = feature_tuple[1]
+            p.z = feature_tuple[2]
+            msg.points.append(p)
         self.laser_features_publisher.publish(msg)
 
 
-    def laser_scan_callback(self,data,pointer):
-        features = []
-        features_idx = []
-        # for idx,point in enumerate(data.points):
-        #     features[idx,:] = [point.x,point.y]
-        #     features_idx[idx] = point.z
-        #
-        #       KALMAN FILTER CODE
-        #
-        pointer.points = data.points
+    def laser_scan_callback(self,data,*args):
+        map_landmarks = args[0][0]
+        pose = args[0][1]
+        orientation_q = pose.pose.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (roll, pitch, yaw) = euler_from_quaternion (orientation_list)
+
+        rho, theta = self.extract_laser_data_from_message(data)
+        x, y = polar2z(rho, theta)
+        map, map_points = self.fd.create_map(x, y)
+        df, img = self.fd.detect_lines(map, plot=False)
+        df = self.fd.check_points_in_line(map_points, df)
+        df_filtered, img = self.fd.filter_segments(df, img)
+        df_inter_filtered, img = self.fd.find_intersections(df_filtered, img)
+        features = self.fd.inter2feature(df_inter_filtered)
+        idx_feature,new_features = self.fm.match_features(
+            features,map_landmarks,
+            features.shape[0],
+            (pose.pose.pose.position.x,pose.pose.pose.position.y,yaw))
+        self.pub_laser_features(features,idx_feature)
+
+
     
-    def Map_features_callback(self,data):
-        pass
-    def EKF_pose_callback(self,data):
-        pass
+    def Map_features_callback(self,data:Marker,pointer):
+        pointer = np.array([])
+        for point in data.points:
+            pointer = np.append(pointer,[point.x,point.y])
+    def EKF_pose_callback(self,data:PoseWithCovarianceStamped,pointer:PoseWithCovarianceStamped):
+        buff = BytesIO()
+        data.serialize(buff)
+        pointer.deserialize(buff.getvalue())
 
 
     def loop(self):
-        last_input = geometry_msgs.msg.Twist()
-        last_features = features_msg()
+        last_pose = PoseWithCovarianceStamped()
+        pose_on_scan = PoseWithCovarianceStamped()
+        last_landmarks = np.array([])
         rospy.init_node('Laser_node', anonymous=True)
-        rospy.Subscriber("/scan", LaserScan, self.laser_scan_callback)
-        rospy.Subscriber("EKF/Landmarks", Marker, self.Map_features_callback)
-        rospy.Subscriber("EKF/pose", PoseWithCovarianceStamped, self.EKF_pose_callback)
-        r = rospy.Rate(1) # 10hz 
-        while not rospy.is_shutdown():
-            #
-            #       KALMAN FILTER CODE
-            #
-            self.pub_estimated_pose([0,0,0],np.zeros((9)))
-            land = np.array([[1,2],[2,1],[1,1],[1.5,1.5]])
-            self.pub_map_landmarks(land)
-
-
-            r.sleep()
+        rospy.Subscriber("/scan", LaserScan, self.laser_scan_callback,(copy.copy(last_landmarks),copy.copy(last_pose)))
+        rospy.Subscriber("EKF/Landmarks", Marker, self.Map_features_callback,last_landmarks)
+        rospy.Subscriber("EKF/pose", PoseWithCovarianceStamped, self.EKF_pose_callback,last_pose)
+        rospy.spin()
 
 if __name__ == '__main__':
-    ekf = EKF_node()
-    ekf.loop()
+    Laser = Laser_node()
+    Laser.loop()
