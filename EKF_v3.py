8b06f0029b3f6ecd3eeb1e5f7454fd6471ca539b
Carlos Henrique Sanz <carloshenriquesanz@tecnico.ulisboa.pt>
2022-06-25 19:18:24 +0100

    aleatorio

--- EKF_v3.py
+++ EKF_v3.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 class EKF():
     def __init__(self):
         self.H = 0 # Jacobian for the observation
-        self.z = 0 # Predicted location of the given landmark 
+        self.z_predict = 0 # Predicted location of the given landmark 
         self.K = 0 # Kalmann Gain
         self.conta_landmarks = 0 
-        self.r = 0
-        self.phi = 0
+        self.r_measure = 0
+        self.phi_measure = 0
 
     def correct_prediction(self, Q, modelo, landmarks, idx_landmarks):
         k = 0
@@ -18,45 +18,74 @@ class EKF():
             j = idx_landmarks[k] 
             self.obs_predict(j, landmarks[k], modelo)
             self.KalmanGain(modelo, Q)
-            land_range_bearing = np.array([self.r, self.phi])
-            modelo.x = modelo.x + self.K @ (land_range_bearing - self.z)
+            land_range_bearing = np.array([self.r_measure, self.phi_measure])
+            # print("Rm", land_range_bearing[0], "Tm", np.rad2deg(land_range_bearing[1]))
+            modelo.x_estimate = modelo.x_estimate + self.K @ (land_range_bearing - self.z_predict)
+            # print("Re", self.z_predict[0], "Te", np.rad2deg(self.z_predict[1]))
+            # print((land_range_bearing[0] - self.z_predict[0]), self.K[0])
+            # print("Raio = ", (self.z_predict[0]), modelo.x[0])
+            # print("Phi = ", (np.rad2deg(self.z_predict[1])))
+
+            # print("Raio = ", (self.z_predict[0] - land_range_bearing[0]), "Kalman = ", self.K[0])
+            # print("Phi = ", (np.rad2deg(self.z_predict[1]- land_range_bearing[1])), "Kalman = ", self.K[1])
+
+            # print((np.rad2deg(land_range_bearing[1]) - np.rad2deg(self.z_predict[1])), self.K[1])
+            # print(self.K @ (land_range_bearing - self.z_predict))
+            # print(((land_range_bearing - self.z_predict)))
+            # print("Ganho Kalman", self.K )
+            # print("Re", self.z_predict[0], "Te", np.rad2deg(self.z_predict[1])))
+            # print("Dif medicao", (land_range_bearixng - self.z_predict))
+            # print("Raio medido = ", self.r, "Raio robo = ", self.z_predict[0])
             l,m = ((self.K @ self.H).shape)
             I = np.identity(l)
-            modelo.sigma = (I - self.K @ self.H) @ modelo.sigma
+            modelo.sigma_estimate = (I - self.K @ self.H) @ modelo.sigma_estimate
             k = k + 1
 
-    def obs_predict(self, j, land_pos, modelo): # Recebe a posicao das features ja no frame global 
-        n = len(modelo.x)
-        x = modelo.x[0]
-        y = modelo.x[1]
-        theta = modelo.x[2]
-        
-        j = int(j)
+        modelo.sigma = modelo.sigma_estimate
+        modelo.x = modelo.x_estimate
 
+    def obs_predict(self, j, land_pos, modelo): 
+        # Recebe a posicao das features ja no frame global, e faz uma predicao de onde eu deveria observar essa landmark 
+        n = len(modelo.x_estimate)
+        x = modelo.x_estimate[0]
+        y = modelo.x_estimate[1]
+        theta = modelo.x_estimate[2]
+        # print(x,y,theta, modelo.x[0], modelo.x[1], modelo.x[2])
+        
+        j = int(j+1)
         land_x = land_pos[0] 
         land_y = land_pos[1]
 
-        land_x_antigo = modelo.x[3 + 2*(j-1)]
-        land_y_antigo = modelo.x[4 + 2*(j-1)]
-
         if (j > self.conta_landmarks):
-            lx = land_x
-            ly = land_y
-            pos_land = np.array([lx, ly])
+            # Se a landmark nunca foi observada, sua posicao e a propria observacao 
+            pos_land = np.array([land_x, land_y])
+            modelo.x[3 + 2*(j-1)] = land_x
+            modelo.x[4 + 2*(j-1)] = land_y
+            modelo.x_estimate[3 + 2*(j-1)] = land_x
+            modelo.x_estimate[4 + 2*(j-1)] = land_y
             self.conta_landmarks = self.conta_landmarks + 1
         else:
-            pos_land = np.array([land_x, land_y])
+            # Se ela ja foi observada, sua posicao e a que esta salva no vetor de estados 
+            land_x_antigo = modelo.x[3 + 2*(j-1)]
+            land_y_antigo = modelo.x[4 + 2*(j-1)]
+            pos_land = np.array([land_x_antigo, land_y_antigo])
 
-        deltax = land_x_antigo - x
-        deltay = land_y_antigo - y
-        self.r = np.sqrt((deltax)**2 + (deltay)**2)
-        self.phi = np.arctan2(deltay, deltax) - theta
+        # Transformando a medida de cartesiano para range bearing
+        deltax = (land_x - x)
+        deltay = (land_y - y)
+        self.r_measure  = np.sqrt((deltax)**2 + (deltay)**2)
+        self.phi_measure  = np.arctan2(deltay, deltax) - theta
+        # print("Xr ", x, "Lx", land_x, "Yr", y, "Ly", land_y)
+        # print("R", self.r_measure, "Phi", np.rad2deg(self.phi_measure))
 
-        pos_mod = np.array([x,y])
-        delta = np.array(pos_land - pos_mod)
+        pos_robo = np.array([x,y])
+        # print("pos", pos_robo[0], pos_land[0])
+        delta = np.array(pos_land - pos_robo)
+        # print("delrta", delta[0], delta[1])
         q = delta.T @ delta
-
-        self.z = np.array([np.sqrt(q), np.arctan2(delta[0], delta[1]) - theta]) 
+        # print(pos_robo[0], pos_land[0])
+        self.z_predict = np.array([np.sqrt(q), np.arctan2(delta[1], delta[0]) - theta]) # prevendo a posicao da landmark  
+        # print("raio = ", self.z_predict[0], "theta = ", np.rad2deg(self.z_predict[1]), np.rad2deg(theta))
 
         h = 1/q * np.array(([-np.sqrt(q) * delta[0], -np.sqrt(q) * delta[1], 0, np.sqrt(q) * delta[0], np.sqrt(q) * delta[0]], 
                     [delta[1], -delta[0], -q, -delta[1], delta[0]]))
@@ -75,13 +104,17 @@ class EKF():
         self.K = sigma @ (H.T) @ np.linalg.inv(H @ sigma @ (H.T) + Q)
 
 class modelo():
-    def __init__(self, x0, dt, sigma0): # x = [x, y, theta], u = [velocity, angular_velocity] 
+    def __init__(self, x0, dt, sigma0, teste): # x = [x, y, theta], u = [velocity, angular_velocity] 
         self.x = x0 # State vector for the model, [x_coord, y_coord, theta, land_1_x, land_1_y, land_2_x, land_2_y ...]
+        self.x_estimate = x0
         self.dt = dt # Time step 
         self.sigma = sigma0 # Covariances matrix 
+        self.sigma_estimate = sigma0
+        self.teste = teste
+        
 
     def move(self, u): # updates de pose and covariance of the robot for a given control input 
-        n = len(self.x)
+        n = len(self.x_estimate)
         F = np.zeros((3,n))
         F[0][0] = 1
         F[1][1] = 1
@@ -90,22 +123,28 @@ class modelo():
         dist  = u[0]*self.dt
         dth = u[1]*self.dt
         dx = F.T @ np.array([np.cos(self.x[2]+ dth)*dist, np.sin(self.x[2]+ dth)*dist, dth])
-        self.x = np.add(self.x, dx)
-        self.update_cov(u) 
-    
+        self.x_estimate = np.add(self.x, dx)
+        # print(self.x, "\n", dx,  "\n",self.x_estimate)
+        
+        if self.teste:
+            self.x = self.x_estimate
 
+        self.update_cov(u) 
+
+    
+ 
     def update_cov(self, u): # updates de covariance matrix for a given input
         # This changes the uncertainty in the robot pose but not in the landmar position
         v = u[0]
         w = u[1]
-        theta = self.x[2]
+        theta = self.x_estimate[2]
         dt = self.dt
-        n = len(self.x)
+        n = len(self.x_estimate)
 
         Res = np.zeros((n,n)) # FALTA COMPLETAR - User defined uncertainty in the range and bearing of the model 
-        Res[0][0] = 0
-        Res[1][1] = 0
-        Res[2][2] = 0
+        Res[0][0] = 1e-3
+        Res[1][1] = 1e-3
+        Res[2][2] = np.deg2rad(0.5)
 
         G = np.identity(n)
         G[0][2] = -np.sin(w * dt + theta) * (v * dt)
@@ -114,7 +153,7 @@ class modelo():
 
 if __name__ == '__main__':
 
-    n = 2 # number of landmarks 
+    n = 1 # number of landmarks 
 
     # inicializa o estado inicial com n*2 landmark e pose (3)
     x0 = np.zeros(n*2 + 3)
@@ -139,9 +178,12 @@ if __name__ == '__main__':
     np.set_printoptions(suppress=True, formatter={'float_kind':'{:16.3f}'.format}, linewidth=10)
     teste = modelo(x0, dt, sigma0)
     teste_ekf = EKF()
-    obs = np.array([[2, 2], [1,1]])
-    teste_ekf.correct_prediction(Q, teste, obs, [1, 2])
-    print(teste.x)
+    obs = np.array([[2, 2]])
+    teste_ekf.correct_prediction(Q, teste, obs, [0])
+
+    # print(np.arctan2(1,1), np.tan(np.arctan2(1,1)))
+    # print(np.arctan2(1,0), np.tan(np.arctan2(1,0)))
+    # print(np.arctan2(0,1), np.tan(np.arctan2(0,1)))
 
 
 
--- microsimulador_ekf.py
+++ microsimulador_ekf.py
@@ -2,6 +2,7 @@ import EKF_v3 as EKF_v3
 from ctypes import sizeof
 import numpy as np
 from random import randint
+from random import seed
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from IPython import display
@@ -16,14 +17,15 @@ class landmark():
 
 plt.close('all')
 
-n = 3 # number of landmarks 
+# l = np.array([[5,5], [0,5], [15,10], [-5,10], [-10,10]]) # Vetor com a posição de todas as landmarks no frame global 
+l = np.array([[5,5]])
+n = len(l) # number of landmarks 
 
 # inicializa o estado inicial com n*2 landmark e pose (3)
 x0 = np.zeros(n*2 + 3)
 x0[0] = 0
 x0[1] = 0
 x0[2] = 0
-noise_fact = 0
 
 # Inicializar as incertezas 
 sigma0 = np.identity(n*2 + 3) * 1e20
@@ -33,11 +35,17 @@ sigma0[2][2] = 0
 
 infinito = 1e20
 Q = np.identity(2) # uncertanty in the measurement, bearing and range 
-Q[0][0] = infinito
-Q[1][1] = infinito
+Q[0][0] = 1e-1
+Q[1][1] = 1e-1
 
-u = [1,0.1]
-dt = 0.5
+u = [1, np.deg2rad(0)]
+dt = 1
+
+mean = 0
+std = 1e-10
+num_samples = 2
+np.random.seed(10) 
+noise_fact = 1e-10
 
 # teste = ekf.modelo(x0, dt, sigma0)
 # teste_ekf = ekf()
@@ -45,7 +53,7 @@ dt = 0.5
 # teste_ekf.correct_prediction(Q, teste, obs, [1, 2])
 # print(teste.x)
 
-np.set_printoptions(suppress=True, formatter={'float_kind':'{:16.3f}'.format}, linewidth=100)
+np.set_printoptions(suppress=True, formatter={'float_kind':'{:16.10f}'.format}, linewidth=250)
 # teste = ekf.modelo(x0, dt, sigma0)
 
 ## Parte para plotar 
@@ -67,12 +75,13 @@ l_yn = []
 fig, ax = plt.subplots()
 # function that draws each frame of the animation
 
-controled_ekf = EKF_v3.modelo(x0, dt, sigma0)
-noise_ekf = EKF_v3.modelo(x0, dt, sigma0)
-real_ekf_model = EKF_v3.modelo(x0, dt, sigma0)
+controled_ekf = EKF_v3.modelo(x0, dt, sigma0, True)
+noise_ekf = EKF_v3.modelo(x0, dt, sigma0, True)
+real_ekf_model = EKF_v3.modelo(x0, dt, sigma0, False)
 real_ekf_filter = EKF_v3.EKF()
 
-l = np.array([[1,2], [3,4], [5,6]]) # Vetor com a posição de todas as landmarks no frame global 
+# l = np.array([[3,3]]) # Vetor com a posição de todas as landmarks no frame global 
+
 land = landmark(l)
 
 def gera_sinal_landmark(ekf, land):
@@ -90,33 +99,36 @@ def gera_sinal_landmark(ekf, land):
         c = np.cos(-theta) 
         s = np.sin(-theta)
         R_frame = np.array([[c, -s], [s, c]])
-
-        land.landmark_r[k] = land.landmark_g[k] @ R_frame.T
-
-        land.landmark_noise_g[k] = land.landmark_g[k] + np.array([randint(-100,100), randint(-100,100)]) * noise_fact
-        land.landmark_noise_r[k] = land.landmark_noise_g[k] @ R_frame.T
-
+        noise_land = np.array(np.random.normal(mean, std, size=num_samples))
+        print("NOISE LANDMARK")
+        land.landmark_r[k] = np.array(land.landmark_g[k] @ R_frame.T)
+        land.landmark_noise_g[k] = np.array(np.add(land.landmark_g[k].astype(float), noise_land.astype(float)))
+        land.landmark_noise_r[k] = np.array(land.landmark_noise_g[k] @ R_frame.T)
+        print(land.landmark_g[k], noise_land, land.landmark_noise_g[k].astype(float))
+    
         k = k + 1
 
 def add_noise(ekf):
-    ekf.x[0] = ekf.x[0] + randint(-100,100) * noise_fact 
-    ekf.x[1] = ekf.x[1] + randint(-100,100) * noise_fact
-    ekf.x[2] = ekf.x[2] + randint(-100,100) * noise_fact
+    ekf.x[0] = ekf.x[0] + randint(-100,100)/100 * noise_fact 
+    ekf.x[1] = ekf.x[1] + randint(-100,100)/100 * noise_fact
+    ekf.x[2] = ekf.x[2] + randint(-100,100)/100 * noise_fact
 
 def animate(i, u, c_ekf, n_ekf, real_ekf_model, real_ekf_filter, land):
 
     c_ekf.move(u)
 
-    add_noise(n_ekf)
-    n_ekf.move(u)
-
-    gera_sinal_landmark(c_ekf, land)
-    real_ekf_filter.correct_prediction(Q, real_ekf_model, land.landmark_noise_r, [0, 1, 2])
+    # add_noise(n_ekf)
+    # n_ekf.move(u)
+    print(i)
+    gera_sinal_landmark(real_ekf_model, land)
     real_ekf_model.move(u)
+    real_ekf_filter.correct_prediction(Q, real_ekf_model, land.landmark_noise_g, range(0,n+1))
 
+    # Landmarks com ruido 
     l_xn = land.landmark_noise_g[:,0]
     l_yn = land.landmark_noise_g[:,1]
 
+    # Landmarks sem ruido 
     l_xc = land.landmark_g[:,0]
     l_yc = land.landmark_g[:,1]
 
@@ -128,21 +140,21 @@ def animate(i, u, c_ekf, n_ekf, real_ekf_model, real_ekf_filter, land):
     x_n.append(n_ekf.x[0])
     y_n.append(n_ekf.x[1])
 
-    # Plota a posicao do robo controlado
+    # Plota a posicao do robo com ekf
     ekf_x.append(real_ekf_model.x[0])
     ekf_y.append(real_ekf_model.x[1])
 
     ax.clear()
 
-    ax.plot(x_c, y_c, label = 'controled')
-    ax.plot(x_n, y_n, label = 'noise')
+    ax.plot(x_c, y_c, label = 'controled', linestyle='dashed')
+    # ax.plot(x_n, y_n, label = 'noise')
     ax.plot(ekf_x, ekf_y, label = 'EKF')
 
     ax.scatter(l_xc, l_yc, label = 'landmark controled', marker = 's')
     ax.scatter(l_xn, l_yn, label = 'landmark noise', marker = 's')
 
-    ax.set_xlim([-20,20])
-    ax.set_ylim([-10,30])
+    ax.set_xlim([-1,20])
+    ax.set_ylim([-1,10])
     ax.set_xlabel('x coordinate [m]')
     ax.set_ylabel('y coordinate [m]')
     ax.legend()
@@ -150,8 +162,7 @@ def animate(i, u, c_ekf, n_ekf, real_ekf_model, real_ekf_filter, land):
     ax.grid()
 
 # run the animation
-ani = FuncAnimation(fig, animate, fargs=(u, controled_ekf, noise_ekf, real_ekf_model, real_ekf_filter, land), frames=130, interval=50, repeat=False)
+ani = FuncAnimation(fig, animate, fargs=(u, controled_ekf, noise_ekf, real_ekf_model, real_ekf_filter, land), frames=20, interval=50, repeat=False)
 ani.save('myAnimation.gif', writer='imagemagick', fps=30)
 # plt.show()
-
 
